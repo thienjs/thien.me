0fb289b6b33b33724283c316756d27cfbb537182
thienjs <thienjsx@gmail.com>
2021-12-09 22:26:09 -0500

    cleanup

---
+++ src/components/TechStack.tsx
@@ -0,0 +1,7 @@
+export default function TechStack() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,7 @@
+export default function TodoList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
