e4a96b387c03306a6971f5a9e31c928176fe2a41
thienjs <thienjsx@gmail.com>
2021-12-13 13:18:01 -0500

    cleanup

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,14 +8,11 @@ const About = () => {
             <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
                 <div className="lg:px-4 lg:mt-12 ">
                     <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
-                        Hey there,
+                        Hey there,I'm Thien 
                     </h1>
                     <div className="mt-6 text-gray-800 dark:text-white">
                         <p className="mb-4">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a
-                            type specimen book.
+                          
                         </p>
                     </div>
                 </div>
