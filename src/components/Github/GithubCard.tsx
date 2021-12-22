a908b319aac1f767da74d452b16a18025d75e806
thienjs <thienjsx@gmail.com>
2021-12-22 00:33:57 -0500

    cleanup

--- src/components/Github/GithubCard.tsx
+++ src/components/Github/GithubCard.tsx
@@ -1,10 +1,10 @@
 import { IRepo } from "~/lib/types";
-import Link from 'next/Link'
+
 
 const GithubCard = (props:IRepo): JSX.Element => {
     return (
       <div>
-        <Link href="/"></Link>
+        
       </div>
     )
 }
