38fb4f7d5ab440015d7d5c76e30236f3320042cb
thienjs <thienjsx@gmail.com>
2021-12-22 01:44:00 -0500

    test

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,7 +1,61 @@
-import slugify from 'slugify'
 
-const token = process.env.GITHUB_TOKEN
+import { GetStaticProps } from 'next'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+  useQuery,
+  gql,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 
-export const getLatestRepos= async (token) => {
-  const response = await
+
+export const getStaticProps: GetStaticProps = async () => {
+  const httpLink = createHttpLink({
+    uri: 'https://api.github.com/graphql',
+  })
+
+  const authLink = setContext((_, { headers }) => {
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+      },
+    }
+  })
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  })
+
+  const { data } = await client.query({
+    query: gql`
+      {
+        user(login: "thienjs") {
+          pinnedItems(first: 6) {
+            edges {
+              node {
+                ... on Repository {
+                  name
+                  url
+                  stargazerCount
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+  })
+
+  const { user } = data
+  const pinnedItems = user.pinnedItems.edges.map(({ node }) => node)
+  return {
+    props: {
+      pinnedItems,
+    },
+  }
 }
