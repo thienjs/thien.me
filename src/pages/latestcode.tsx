b7b38e2cc6366f2d1625b229a9d847aaaaf05660
thienjs <thienjsx@gmail.com>
2021-12-28 11:46:47 -0500

    clean up

--- src/pages/latestcode.tsx
+++
@@ -1,8 +0,0 @@
-export default function latestcode() {
-    return (
-        <div>
-                    <h1 className='text-5xl font-semibold py-2 mb-4 '>Latest Code</h1>
-        <p className=''>Coming Soon</p>
-        </div>
-    )
-}
--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -1,7 +1,6 @@
 import NowPlaying from "~/components/music/NowPlaying"
 import TopTracks from "~/components/music/TopTracks"
 import Layout from "~/components/ui/Layout"
-import { getNowPlaying } from "~/lib/spotify"
 export default function MusicPage() {
     return (
         <Layout>
