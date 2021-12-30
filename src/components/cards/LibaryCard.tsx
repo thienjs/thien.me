82083ba6e9a7d161afda246efab31f4f831d503a
thienjs <thienjsx@gmail.com>
2021-12-29 21:54:05 -0500

    clean up

--- src/components/cards/LibaryCard.tsx
+++
@@ -1,5 +0,0 @@
-
-import clsx from 'clsx';
-import * as React from 'react';
-import { GiTechnoHeart } from 'react-icons/gi';
-import TechIcons, { TechListType } from '~/components/icons/tech-icons/techIcons';
