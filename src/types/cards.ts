1a37592bfabd6a99d6c0ee00c9486378a2b74e97
thienjs <thienjsx@gmail.com>
2021-12-30 09:51:36 -0500

    cleanup

--- src/types/cards.ts
+++
@@ -1,5 +0,0 @@
-export type NotionCard = {
-    title: string
-    
-
-}
--- src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export type Todo = {
-  id: number;
-  inserted_at: string;
-  is_complete: boolean;
-  task: string;
-  user_id: string;
-};
