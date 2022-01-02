e40bd38c5713dbe9d7edd11835245055bd090d54
thienjs <thienjsx@gmail.com>
2022-01-01 22:42:17 -0500

    happy new year

--- next.config.js
+++ next.config.js
@@ -11,6 +11,8 @@ module.exports = {
     domains: [
       'i.scdn.co', // Spotify Album Art
       'pbs.twimg.com',
+      'avatars.githubusercontent.com',
+      'github.com',
       's3.us-west-2.amazonaws.com', // Images coming from Notion
       'via.placeholder.com', // for articles that do not have a cover image
       'images.unsplash.com', // For blog posts that use an external cover image
@@ -18,8 +20,8 @@ module.exports = {
       'dwgyu36up6iuz.cloudfront.net',
       'cdn.hashnode.com',
       'res.craft.do',
-      'res.cloudinary.com' // Twitter Profile Picture
-    ]
+      'res.cloudinary.com', // Twitter Profile Picture
+    ],
   },
   webpack: (config, { dev, isServer }) => {
     // Replace React with Preact only in client production build
@@ -27,10 +29,10 @@ module.exports = {
       Object.assign(config.resolve.alias, {
         react: 'preact/compat',
         'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      });
+        'react-dom': 'preact/compat',
+      })
     }
 
-    return config;
-  }
-};
+    return config
+  },
+}
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,44 +4,46 @@ import { motion } from "framer-motion"
 
 export default function Hero() {
   return (
-        <div>
-            <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse lg:text-left">
-                <div className="lg:px-4 lg:mt-12 ">
-                    <motion.div initial="hidden" animate="visible" variants={{
-                      hidden: {
-                        scale: .8,
-                        opacity: 0,
-                      },
-                      visible: {
-                        scale: 1,
-                        opacity: 1
-                      }
-                    } }>
-                      
-                    <h1 className="text-2xl font-bold text-gray-900 lg:text-4xl dark:text-white">
-                        Hey there,I'm Thien 
-                    <p className="wave"> 👋</p>
-                    </h1>
-                    </motion.div>
-                    <div className="mt-6 text-gray-800 dark:text-white">
-                        <p className="mb-4 pb-6">
-                          Full Stack Web Devloper | Site underconstruction.
-                        </p>
-                    </div>
-                </div>
-                <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10 hidden">
-                    <Image
-                        src={profile}
-                        alt="Profile"
-                        priority={true}
-                        className="rounded-full"
-                        width={150}
-                        height={150}
-                        placeholder="blur"
-                    />
-                </div>
-            </div>
-            <p></p>
+    <div>
+      <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse lg:text-left">
+        <div className="lg:px-4 lg:mt-12 ">
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                scale: 0.8,
+                opacity: 0,
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+              },
+            }}
+          >
+            <h1 className="text-2xl font-bold text-gray-900 lg:text-4xl dark:text-white">
+              Hey there,I'm Thien
+              <p className="wave"> 👋</p>
+            </h1>
+          </motion.div>
+          <div className="mt-6 text-gray-800 dark:text-white">
+            <p className="mb-4 pb-6">
+              Full Stack Web Devloper | Site underconstruction.
+            </p>
+          </div>
         </div>
+        <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10 ">
+          <Image
+            src="https://github.com/thienjs.png"
+            alt="Profile"
+            priority={true}
+            className="rounded-full border border-white"
+            width={150}
+            height={150}
+          />
+        </div>
+      </div>
+      <p></p>
+    </div>
   )
 }
