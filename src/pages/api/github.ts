db7febdf8a093b48f2ee5a1a963eccae5cb830d8
thienjs <thienjsx@gmail.com>
2022-01-05 15:30:55 -0500

    cleanup

--- src/pages/api/github.ts
+++ src/pages/api/github.ts
@@ -1,45 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-const { Octokit } = require('@octokit/rest')
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_AUTH_TOKEN,
-  })
 
   const headers = {
     Authorization: 'Token ' + process.env.GITHUB_AUTH_TOKEN,
   }
 
-  //Followers request
-  const followers = await octokit.request(
-    '/users/thienjs/followers?per_page=100'
-  )
-  const followerCount = followers.data.length
-
-  //Last updated request
-  const portfolio = await octokit.request('/repos/thienjs/thien.me')
-  const portfolioUpdated = portfolio.data.pushed_at
-
-  //Stars request
-  const stars = await octokit.request('/users/thienjs/repos')
-  const starsCount = stars.data
-    .filter((repo) => !repo.fork)
-    .reduce((acc, item) => {
-      return acc + item.stargazers_count
-    }, 0)
-
-  //Repos request
-  const reposStarred = await octokit.request(
-    '/users/thienjs/starred?per_page=100'
-  )
-  const starredCount = reposStarred.data.length
-
-  //Orgs request
-  const userOrgs = await octokit.request('/user/orgs')
-  const orgsCount = userOrgs.data.length
 
   // projects
   const url = 'https://api.github.com/users/thienjs/repos?per_page=10'
@@ -59,12 +28,5 @@ export default async function handler(
   })
 
   //return
-  return res.status(200).json({
-    stars: starsCount,
-    followers: followerCount,
-    starred: starredCount,
-    orgsCont: orgsCount,
-    repos: projectsList,
-    portfolioLastUpdated: portfolioUpdated,
-  })
+
 }
