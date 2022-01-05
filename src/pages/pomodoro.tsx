6ce6111be59bfd2a9fcccb3979e93f78d17968a5
thienjs <thien.tran6@gmail.com>
2022-01-05 00:41:29 -0500

    pomodoro page

---
+++ src/pages/pomodoro.tsx
@@ -0,0 +1,9 @@
+import Layout from "~/components/ui/Layout"
+import Pomodoro from "~/components/clock/Pomodoro"
+export default function PomodoroPage() {
+  return (
+    <Layout>
+      <Pomodoro/>
+    </Layout>
+  )
+}
