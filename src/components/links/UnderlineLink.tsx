e8a30e97d94f66206d0a07a346f7d6a60804579f
thienjs <thienjsx@gmail.com>
2022-01-05 14:59:53 -0500

    added links components

---
+++ src/components/links/UnderlineLink.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+import clsxm from '~/lib/clsxm';
+
+import UnstyledLink, {
+  UnstyledLinkProps,
+} from '~/components/links/UnstyledLink';
+
+export default function UnderlineLink({
+  children,
+  className,
+  ...rest
+}: UnstyledLinkProps) {
+  return (
+    <UnstyledLink
+      {...rest}
+      className={clsxm(
+        'animated-underline custom-link inline-flex items-center font-semibold',
+        'focus:outline-none focus-visible:rounded focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-offset-2',
+        'border-dark border-b border-dotted hover:border-black/0',
+        className
+      )}
+    >
+      {children}
+    </UnstyledLink>
+  );
+}
--- src/components/links/UnstyledLink.tsx
+++ src/components/links/UnstyledLink.tsx
@@ -1,19 +1,21 @@
-import clsx from 'clsx';
 import Link, { LinkProps } from 'next/link';
+
+import clsxm from '~/lib/clsxm';
 
 export type UnstyledLinkProps = {
   href: string;
   children: React.ReactNode;
   openNewTab?: boolean;
   className?: string;
-} & React.ComponentPropsWithoutRef<'a'> &
-  LinkProps;
+  nextLinkProps?: Omit<LinkProps, 'href'>;
+} & React.ComponentPropsWithoutRef<'a'>;
 
 export default function UnstyledLink({
   children,
   href,
   openNewTab,
   className,
+  nextLinkProps,
   ...rest
 }: UnstyledLinkProps) {
   const isNewTab =
@@ -23,7 +25,7 @@ export default function UnstyledLink({
 
   if (!isNewTab) {
     return (
-      <Link href={href}>
+      <Link href={href} {...nextLinkProps}>
         <a {...rest} className={className}>
           {children}
         </a>
@@ -37,7 +39,7 @@ export default function UnstyledLink({
       rel='noopener noreferrer'
       href={href}
       {...rest}
-      className={clsx(className, 'cursor-newtab')}
+      className={clsxm('cursor-newtab', className)}
     >
       {children}
     </a>
