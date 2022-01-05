0550ea83456b4b39c33b0a9374cf015473f6fd9f
thienjs <thien.tran6@gmail.com>
2022-01-05 16:54:48 -0500

    stats

---
+++ src/pages/stats.tsx
@@ -0,0 +1,9 @@
+import Layout from "~/components/ui/Layout"
+import { TotalArticles } from "~/components/stats/TotalArticles"
+export default function Stats() {
+  return (
+    <Layout>
+      <TotalArticles/>
+    </Layout>
+  )
+}
