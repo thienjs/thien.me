1c0994ed73160141df54966e20c2e80d08148bb2
thienjs <thienjsx@gmail.com>
2022-01-10 16:12:15 -0500

    about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,10 +3,6 @@ import Layout from '~/components/ui/Layout'
 export default function AboutPage() {
     return (
       <Layout>
-        <h1 className="text-5xl font-bold py-4 text-transparent bg-clip-text bg-gradient-to-b from-cyan-400 to-pink-500">
-          About
-        </h1>
-        <p className="">This is where i gather my thoughts</p>
         <About />
       </Layout>
     )
