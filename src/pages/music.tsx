92e96a2e2702fd0635cdf4134c9469a8e72a939d
thienjs <thien.tran6@gmail.com>
2022-01-12 00:15:46 -0500

    siggh

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -1,6 +1,5 @@
 import NowPlaying from "~/components/music/NowPlaying"
 import TopTracks from "~/components/music/TopTracks"
-import Tracks from '~/components/Tracks'
 import Layout from "~/components/ui/Layout"
 export default function MusicPage() {
     return (
@@ -10,7 +9,7 @@ export default function MusicPage() {
             <h2 className="text-2xl font-semibold mb-3 ">Currently listening to:</h2>
             <NowPlaying/>
             <h2 className="text-2xl font-semibold">My Top 10 recently:</h2>
-            <Tracks/>
+
             <TopTracks/>
 
         </Layout>
