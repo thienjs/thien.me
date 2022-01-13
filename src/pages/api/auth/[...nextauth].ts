65e1cc84257161d71935ed637cc9c3ff57708635
thienjs <thien.tran6@gmail.com>
2022-01-13 17:08:42 -0500

    test

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,17 +2,27 @@ import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import SpotifyProvider from 'next-auth/providers/spotify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
+    SpotifyProvider({
+      authorization:
+        'https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private',
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    }),
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_KEY,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-  ]
-});
+  ],
+})
