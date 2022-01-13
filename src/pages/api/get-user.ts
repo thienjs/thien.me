913dc866a1d6720e5f5076a7cee3ccb8f50458db
thienjs <thien.tran6@gmail.com>
2022-01-13 16:50:51 -0500

    idk

--- src/pages/api/get-user.ts
+++
@@ -1,6 +0,0 @@
-import { supabase } from "~/lib/supabase";
-
-export default async function getUser(req, res) {
-  const user = await supabase.auth.user();
-  return res.status(200).json({ user: user });
-}
