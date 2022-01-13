3a4b941fd14982242f8721f20d6d3a0b316994ba
thienjs <thien.tran6@gmail.com>
2022-01-13 18:14:20 -0500

    test

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,20 +1,22 @@
 import {useSession, signIn, signOut} from 'next-auth/react';
+import Layout from '~/components/ui/Layout';
+import ButtonLink from '~/components/links/ButtonLink';
 
 export default function Home() {
   const {data: session} = useSession();
 
   if (session) {
     return (
-      <>
+      <Layout>
         Signed in as {session?.token?.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
-      </>
+      </Layout>
     );
   }
   return (
-    <>
+    <Layout>
       Not signed in <br />
       <button onClick={() => signIn()}>Sign in</button>
-    </>
+    </Layout>
   );
 }
