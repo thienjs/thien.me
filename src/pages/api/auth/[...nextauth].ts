74863a735ef8c00d3a4873cad357b5f786116bf3
thienjs <thienjsx@gmail.com>
2022-01-18 16:11:29 -0500

    working

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,17 +7,13 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import EmailProvider from 'next-auth/providers/email'
 import { NextApiHandler } from 'next'
 
-const prisma = new PrismaClient()
 
-const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options)
-export default authHandler
-
-const options = {
+export default NextAuth({
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -28,8 +24,9 @@ const options = {
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
     }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
   ],
-  adapter: PrismaAdapter(prisma),
-
-  secret: process.env.NEXT_AUTH_SECRET,
-}
+})
