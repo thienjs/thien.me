6473bdc0446776556ecd1bb71bb2b6e5c4c7a08e
thienjs <thien.tran6@gmail.com>
2022-01-18 17:35:31 -0500

    sighh

---
+++ src/pages/p/[id].ts
@@ -0,0 +1,39 @@
+import React from 'react'
+import { GetServerSideProps } from 'next'
+import ReactMarkdown from 'react-markdown'
+import Layout from '~/components/ui/Layout'
+import Router from 'next/router'
+import { PostProps } from '~/components/Post'
+import {prisma} from '~/lib/prisma'
+import { useSession } from 'next-auth/react'
+import { DividerHorizontalIcon } from '@radix-ui/react-icons'
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: Number(params?.id) || -1,
+    },
+    include: {
+      author: {
+        select: { name: true, email: true },
+      },
+    },
+  })
+  return {
+    props: post,
+  }
+}
+
+async function publishPost(id: number): Promise<void> {
+  await fetch(`/api/publish/${id}`, {
+    method: 'PUT',
+  })
+  await Router.push('/')
+}
+
+async function deletePost(id: number): Promise<void> {
+  await fetch(`/api/post/${id}`, {
+    method: 'DELETE',
+  })
+  await Router.push('/')
+}
