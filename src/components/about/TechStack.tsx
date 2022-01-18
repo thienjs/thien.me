ffa6cc013255ffe80d3c6b9e333345aedd629f83
thienjs <thien.tran6@gmail.com>
2022-01-18 01:08:50 -0500

    more stack

--- src/components/about/TechStack.tsx
+++ src/components/about/TechStack.tsx
@@ -15,28 +15,32 @@ import {
   SiSass,
   SiTailwindcss,
   SiTypescript,
+  SiPrisma,
+  SiSupabase,
 } from 'react-icons/si'
 
 export default function TechStack() {
   return (
     <div className='font-semibold '>
       <h2>My Current Stack</h2>
-    <div className="flex my-6 flex-wrap justify-evenly h-18 ">
+    <div className="flex my-6 flex-wrap justify-evenly h-18 text-zinc-700 dark:text-gray-200 ">
       <SiTypescript size={40} />
       <SiTailwindcss size={40}/>
       <SiNotion  size={40}/>
       <SiReact size={40}/>
       <SiNextdotjs size={40} />
       <IoLogoVercel size={40} />
-      <SiMarkdown size={40} />
       <SiNodedotjs size={40} />
+      <SiPrisma size={40}/>
+      <SiSupabase size={40}/>
     </div>
     <h2>Other Tech Skills</h2>
-    <div className='flex my-6 justify-evenly'>
+    <div className='flex my-6 justify-evenly text-zinc-700 dark:text-gray-200 '>
       <SiJavascript size={40} />
       <SiPrettier size={40}/>
       <SiGit size={40} />
       <SiRedux size={40}/>
+      <SiMarkdown size={40} />
       <SiMongodb size={40} />
       <SiGoogleanalytics size={40} />
 
