d0a1a035e84a2c704d05bbebd587b68f806709b4
thienjs <thien.tran6@gmail.com>
2022-01-18 23:13:47 -0500

    styling and prisma migration

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -24,15 +24,16 @@ const Draft: React.FC = () => {
 
   return (
     <Layout>
-      <div>
-        <form onSubmit={submitData}>
-          <h1>New Draft</h1>
+      <div className="flex flex-col">
+        <form className="flex flex-col" onSubmit={submitData}>
+          <h1 className="mb-2 font-semibold text-lg">New Draft</h1>
           <input
             autoFocus
             onChange={(e) => setTitle(e.target.value)}
             placeholder="Title"
             type="text"
             value={title}
+            className='text-gray-600 dark:text-gray-100 dark:bg-zinc-400 mb-3'
           />
           <textarea
             cols={50}
@@ -40,41 +41,18 @@ const Draft: React.FC = () => {
             placeholder="Content"
             rows={8}
             value={content}
+            className='text-gray-600 dark:text-gray-100 dark:bg-zinc-400'
           />
-          <input type="submit" value="Create" />
-          <a className="back" href="#" onClick={() => Router.push("/")}>
+          <div className="flex justify-between mt-3">
+          <input type="submit" value="Create" className="hover:text-cyan-500 px-6 py-2 border rounded-md bg-cyan-600 dark:bg-cyan-800"/>
+          <a className="" href="#" onClick={() => Router.push("/")}>
             or Cancel
           </a>
+
+          </div>
         </form>
       </div>
-      <style jsx>{`
-        .page {
-          background: white;
-          padding: 3rem;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        input[type="text"],
-        textarea {
-          width: 100%;
-          padding: 0.5rem;
-          margin: 0.5rem 0;
-          border-radius: 0.25rem;
-          border: 0.125rem solid rgba(0, 0, 0, 0.2);
-        }
-
-        input[type="submit"] {
-          background: #ececec;
-          border: 0;
-          padding: 1rem 2rem;
-        }
-
-        .back {
-          margin-left: 1rem;
-        }
-      `}</style>
+     
     </Layout>
   );
 };
