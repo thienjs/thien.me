d89735fa4a8d6f58489db7db48ab7117e4500a74
thienjs <thien.tran6@gmail.com>
2022-01-18 23:47:38 -0500

    basepath error in production

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -11,7 +11,7 @@ const Draft: React.FC = () => {
     e.preventDefault();
     try {
       const body = { title, content };
-      await fetch(`${basePath}/api/post`, {
+      await fetch(`/api/post`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
--- src/pages/p/[id].tsx
+++ src/pages/p/[id].tsx
@@ -25,14 +25,14 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 }
 
 async function publishPost(id: number): Promise<void> {
-  await fetch(`${basePath}/api/publish/${id}`, {
+  await fetch(`/api/publish/${id}`, {
     method: 'PUT',
   })
   await Router.push('/')
 }
 
 async function deletePost(id: number): Promise<void> {
-  await fetch(`${basePath}/api/post/${id}`, {
+  await fetch(`/api/post/${id}`, {
     method: 'DELETE',
   })
   await Router.push('/')
