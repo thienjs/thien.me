477928d8258722246dac0bb3560f97798c7ab415
thienjs <thienjsx@gmail.com>
2022-01-20 13:46:10 -0500

    add auth providers

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,15 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaClient } from "@prisma/client"
+import BattleNetProvider from 'next-auth/providers/battlenet'
+import TwitterProvider from 'next-auth/providers/twitter'
+import RedditProvider from 'next-auth/providers/reddit'
+import DiscordProvider from 'next-auth/providers/discord'
+import { PrismaClient } from '@prisma/client'
 import SpotifyProvider from 'next-auth/providers/spotify'
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import EmailProvider from 'next-auth/providers/email'
 import { NextApiHandler } from 'next'
-
 
 export default NextAuth({
   session: {
@@ -28,5 +31,23 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
+    BattleNetProvider({
+      clientId: process.env.BATTLENET_CLIENT_ID,
+      clientSecret: process.env.BATTLENET_CLIENT_SECRET,
+      region: process.env.BATTLENET_REGION,
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
+      version: '2.0',
+    }),
+    RedditProvider({
+      clientId: process.env.REDDIT_CLIENT_ID,
+      clientSecret: process.env.REDDIT_CLIENT_SECRET
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET
+    })
   ],
 })
