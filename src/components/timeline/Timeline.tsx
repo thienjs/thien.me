72faea9bf0db70c05e7e7c6147f0c42ee9bf3e80
thienjs <thienjsx@gmail.com>
2022-01-31 13:18:55 -0500

    timeline

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,16 +1,19 @@
 import { RiAncientGateFill } from "react-icons/ri";
-import { FaGraduationCap } from 'react-icons/fa'
+import { FaGraduationCap, FaDog, FaBlackTie } from 'react-icons/fa'
+import { AiOutlineCode } from 'react-icons/ai'
+import { SiAol } from 'react-icons/si'
 
 export default function Timeline() {
   return (
     <div className="">
-      <h2 className="font-bold text-2xl mt-6 ">My Journey</h2>
-      <ol className="relative border-l border-gray-200 dark:border-gray-700 mt-6 ml-6">
-        <li className="mb-10 ml-6">
-          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-green-900">
-            <RiAncientGateFill />
+      <h2 className="font-bold text-3xl mt-6 ">My Journey</h2>
+      <ol className="relative border-l border-gray-200 dark:border-gray-700 mt-6 ml-6 ">
+        <li className="mb-4 ml-8 px-4 py-4 bg-gray-100 dark:bg-zinc-900  border  rounded-md border-gray-200 dark:border-gray-800">
+          <span className=" animate-pulse absolute flex items-center justify-center w-6 h-6 bg-blue-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-green-900">
+            <FaBlackTie className="animate-none" />
           </span>
-          <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+
+          <h3 className="flex items-center text-base  font-semibold text-gray-900 dark:text-white">
             Applying for Jobs
             <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
               Present
@@ -19,81 +22,66 @@ export default function Timeline() {
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             January 13th, 2022
           </time>
-          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          <p className=" text-sm font-normal text-gray-500 dark:text-gray-400">
             Feel confident I can work as a junior web developer. Wish me Luck
           </p>
-          <a
-            href="#"
-            className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-          >
-            <svg
-              className="w-4 h-4 mr-2"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>{' '}
-            Download Resume
-          </a>
         </li>
-        <li className="mb-10 ml-6">
-          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <svg
-              className="w-3 h-3 text-blue-600 dark:text-blue-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
+        <li className="mb-4 ml-8 px-4 py-4 bg-gray-100 dark:bg-zinc-900  border  rounded-md border-gray-200 dark:border-gray-800">
+          <span className="absolute flex items-center justify-center w-6 h-6 bg-red-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-red-900">
+            <FaDog />
           </span>
-          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+          <h3 className="mb-1 text-base font-semibold text-gray-900 dark:text-white">
+            Adopted Loki
+          </h3>
+          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            July 20th, 2021
+          </time>
+          <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
+            My crazy 4 legged best friend
+          </p>
+        </li>
+        <li className="mb-4 ml-8 px-4 py-4 bg-gray-100 dark:bg-zinc-900  border  rounded-md border-gray-200 dark:border-gray-800">
+          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <AiOutlineCode />
+          </span>
+          <h3 className="mb-1 text-base font-semibold text-gray-900 dark:text-white">
             Started Learning Javascript Online
           </h3>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             September 6th, 2019
           </time>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+          <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
             <p>- Angela Yu web development</p>
             <p>- Brad Traversy Javascript</p>
             <p>- colt steele react</p>
           </p>
         </li>
-        <li className="mb-10 ml-6">
-          <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+        <li className="mb-4 ml-8 px-4 py-4 bg-gray-100 dark:bg-zinc-900  border  rounded-md border-gray-200 dark:border-gray-800">
+          <span className="absolute flex items-center justify-center w-6 h-6 bg-purple-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-purple-900">
             <FaGraduationCap />
           </span>
-          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+          <h3 className="mb-1 text-base font-semibold text-gray-900 dark:text-white">
             Graduated From University of Connecticut
           </h3>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             May 16th, 2012
           </time>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+          <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
             Bachelor of Arts in Business Finance
           </p>
         </li>
-        <li className="mb-10 ml-6">
+        <li className="mb-4 ml-8 px-4 py-4 bg-gray-100 dark:bg-zinc-900  border  rounded-md border-gray-200 dark:border-gray-800">
           <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <FaGraduationCap />
+            <SiAol />
           </span>
-          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+          <h3 className="mb-1 text-base font-semibold text-gray-900 dark:text-white">
             Connectected to the Internet
           </h3>
           <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             1999
           </time>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            Still hear that dialup
+          <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
+            beeeepeeppeboopeeeepp
           </p>
         </li>
       </ol>
