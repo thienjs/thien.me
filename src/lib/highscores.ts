607fdd3efe47a5579e7b78ad6929e87ee7954d8d
thienjs <thienjsx@gmail.com>
2022-02-02 13:02:49 -0500

    highscore start

---
+++ src/lib/highscores.ts
@@ -0,0 +1,13 @@
+import { prisma } from './prisma';
+
+export type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
+export type ScoresAll = ThenArg<ReturnType<typeof fetchHighScores>>;
+
+export const fetchHighScores = async () => {
+  const entries = await prisma.guestbook.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+  return entries;
+};
---
+++ src/pages/highscore.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function highscore() {
+  return <div>highscore</div>;
+}
