fbc516195dd4f74aba67c1803ae183d48c4c4848
thienjs <thien.tran6@gmail.com>
2022-02-01 20:56:39 -0500

    w-full

--- src/components/guestbook/ContentPopover/ContentInput.tsx
+++ src/components/guestbook/ContentPopover/ContentInput.tsx
@@ -4,42 +4,42 @@ import * as React from 'react';
 import useSWR, { useSWRConfig } from 'swr';
 import { signOut } from 'next-auth/react';
 import toast from 'react-hot-toast';
+import { BiFullscreen } from 'react-icons/bi'
 
 export enum Form {
   Initial,
-  Loading
+  Loading,
 }
-export type FormState = Form;
+export type FormState = Form
 
 export function ContentInput() {
-  const [form, setForm] = React.useState<FormState>(Form.Initial);
-  const inputEl = React.useRef<HTMLTextAreaElement>(null);
-  const { mutate } = useSWRConfig();
+  const [form, setForm] = React.useState<FormState>(Form.Initial)
+  const inputEl = React.useRef<HTMLTextAreaElement>(null)
+  const { mutate } = useSWRConfig()
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setForm(Form.Loading);
-    if (!inputEl.current) return;
+    e.preventDefault()
+    setForm(Form.Loading)
+    if (!inputEl.current) return
     const req = await fetch('/api/guestbook/sign', {
       body: JSON.stringify({
-        message: inputEl?.current?.value
+        message: inputEl?.current?.value,
       }),
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      method: 'POST'
-    });
-    const res = await req.json();
+      method: 'POST',
+    })
+    const res = await req.json()
     if (res.success) {
-      inputEl.current.value = '';
-      mutate('/api/guestbook/all');
-      toast.success('Hooray! Thanks for signing my Guestbook.');
+      inputEl.current.value = ''
+      mutate('/api/guestbook/all')
+      toast.success('Hooray! Thanks for signing my Guestbook.')
     }
     if (!res.success) {
-      toast.error(res.message);
+      toast.error(res.message)
     }
-    setForm(Form.Initial);
-
-  };
+    setForm(Form.Initial)
+  }
   return (
     <div className="flex flex-col ">
       <h2 className="text-lg font-bold">Write a message</h2>
@@ -51,10 +51,9 @@ export function ContentInput() {
           Your Message
         </label>
         <textarea
-          className="rounded-md border border-gray-300 text-sm shadow-sm focus:border-gray-500 focus:ring-gray-500 dark:bg-gray-800 dark:text-white dark:focus:border-gray-300 dark:focus:ring-gray-300"
+          className="w-full rounded-md border border-gray-300 text-sm shadow-sm focus:border-gray-500 focus:ring-gray-500 dark:bg-gray-800 dark:text-white dark:focus:border-gray-300 dark:focus:ring-gray-300"
           ref={inputEl}
           rows={1}
-          cols={46}
           aria-label="Your message"
           placeholder="Your message..."
           required
@@ -83,12 +82,12 @@ export function ContentInput() {
       </form>
       <button
         onClick={() => {
-          signOut();
+          signOut()
         }}
         className="mt-2 w-full rounded bg-red-200 px-3 py-1 font-medium ring-red-300 transition-all hover:ring-2 dark:bg-red-600"
       >
         Log out
       </button>
     </div>
-  );
+  )
 }
