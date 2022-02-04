d1adc8301b555617cf2f06f3c57550b4553a6966
thienjs <thien.tran6@gmail.com>
2022-02-03 21:11:06 -0500

    reset

--- src/pages/api/contact/submit.ts
+++ src/pages/api/contact/submit.ts
@@ -14,57 +14,48 @@ type SheetForm = {
     phone: string
     message: string
 }
-const { data, error } = useSWR(
-  ['../decrypt', service.encrypted],
-  fetchWithData
-)
+
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
- 
+  if (req.method !== 'POST') {
+    return res.status(405).send({ message: 'Only POST requests allowed' })
+  }
 
+  const body = req.body as SheetForm
 
-    if (req.method !== 'POST') {
-        return res.status(405).send({ message: 'Only POST requests allowed' })
-    }
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+      },
+      scopes: [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.file',
+        'https://www.googleapis.com/auth/spreadsheets',
+      ],
+    })
 
-    const body = req.body as SheetForm
+    const sheets = google.sheets({
+      auth,
+      version: 'v4',
+    })
 
-    try {
-        const auth = new google.auth.GoogleAuth({
-            credentials: {
-                client_email: process.env.GOOGLE_CLIENT_EMAIL,
-                private_key: data.private_key
-            },
-            scopes: [
-                'https://www.googleapis.com/auth/drive',
-                'https://www.googleapis.com/auth/drive.file',
-                'https://www.googleapis.com/auth/spreadsheets'
-            ]
-        })
+    const response = await sheets.spreadsheets.values.append({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      range: 'A1:D1',
+      valueInputOption: 'USER_ENTERED',
+      requestBody: {
+        values: [[body.name, body.email, body.phone, body.message]],
+      },
+    })
 
-        const sheets = google.sheets({
-            auth,
-            version: 'v4'
-        });
-
-        const response = await sheets.spreadsheets.values.append({
-            spreadsheetId: process.env.GOOGLE_SHEET_ID,
-            range: 'A1:D1',
-            valueInputOption: 'USER_ENTERED',
-            requestBody: {
-                values: [
-                    [body.name, body.email, body.phone, body.message]
-                ]
-            }
-        });
-
-        return res.status(201).json({
-            data: response.data
-        })
-    }catch (e) {
-        return res.status(e.code).send({message: e.message})
-    }
-
+    return res.status(201).json({
+      data: response.data,
+    })
+  } catch (e) {
+    return res.status(e.code).send({ message: e.message })
+  }
 }
