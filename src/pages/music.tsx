96a71499bc5df752d49036225fdce63b7823b462
thienjs <thienjsx@gmail.com>
2022-02-05 18:14:13 -0500

    trying to deploy

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -6,7 +6,7 @@ export default function MusicPage() {
         <Layout>
             <h1 className='text-5xl font-semibold py-2 mb-4 '>Music</h1>
 
-            <h2 className="text-2xl font-semibold mb-3 ">Currently listening to:</h2>
+            <h2 className="text-2xl font-semibold mb-2 ">Currently listening to:</h2>
             <NowPlaying/>
             <h2 className="text-2xl font-semibold">My Top 10 recently:</h2>
 
