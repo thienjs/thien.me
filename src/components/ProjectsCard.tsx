e2910d9bc8d48375f266a5576b12261786368b90
thienjs <thienjsx@gmail.com>
2022-02-05 17:49:53 -0500

    vercel not deploying

--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -5,7 +5,7 @@ import { Project, projects } from 'data/projects'
 
 const ProjectCard = ({ title, live, image }: Project) => {
   return (
-    <div className="border-2 p-4 rounded-md border-opacity-60 dark:border-cyan-500 border-gray-200 max-w-3xl mb-2">
+    <div className="border-2 p-3 rounded-md border-opacity-60 dark:border-cyan-500 border-gray-200 max-w-3xl mb-2">
     <div className="flex flex-col">
       <Link href="/">
         <a>{title}</a>
