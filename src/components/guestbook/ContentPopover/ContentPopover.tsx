fc40ee3477299dcfb0efaa476e4f853244bb74a6
thienjs <thienjsx@gmail.com>
2022-02-06 22:47:43 -0500

    chore

--- src/components/guestbook/ContentPopover/ContentPopover.tsx
+++ src/components/guestbook/ContentPopover/ContentPopover.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as PopoverPrimitive from '@radix-ui/react-popover';
+
 import { FiDivide, FiX } from 'react-icons/fi'
 import { AuthButtons } from './AuthButtons'
 import { ContentInput } from './ContentInput'
