fd925da4863100645270deb108a5edb707ebdd73
thienjs <thien.tran6@gmail.com>
2022-02-12 22:07:45 -0500

    article card styles

--- src/components/blog/ArticleCard.tsx
+++ src/components/blog/ArticleCard.tsx
@@ -33,54 +33,8 @@ export function ArticleCard({ article }: Props) {
       onClick={() => handleArticleClicked(slug)}
     >
       <div className="flex flex-col">
-        <div className="text-md font-semibold text-left mb-1 ">
+        <div className="flex text-md font-semibold text-left mb-1 justify-between ">
           {article.title}
-        </div>
-        {/* {JSON.stringify(article)} */}
-        {/* <p>{article.summary}</p> */}
-        <p className="pb-2 text-left text-gray-600 dark:text-gray-400  text-sm">
-          {article.summary}
-        </p>
-        <span className="text-xs flex text-gray-600 dark:text-gray-400 ">
-          {new Date(article.publishedDate).toLocaleDateString(
-            "ko-KR",
-            {
-              year: 'numeric',
-              month: '2-digit',
-              day: '2-digit',
-            }
-          )}{' '}
-          <div className="flex  text-gray-800 dark:text-gray-200 capsize text-xs items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-3.5 w-3.5 ml-4 "
-              fill="none"
-              viewBox="0 0 22 22"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={1}
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={1}
-                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-              />
-            </svg>
-            <span className="ml-1 text-xs text-gray-800 dark:text-gray-400 font-light">
-              {views ? new Number(views).toLocaleString() : '–––'}
-            </span>
-          </div>
-          <div className="ml-6 flex items-center ">
-            <div className='mr-1 '>
-              <FiClock />
-            </div>
-            {readingTimeStats.words} mins
-          </div>
           {hasRead && (
             <span className="text-sm inline-flex items-center text-gray-800 dark:text-gray-400 opacity-75  ml-6">
               <svg width="16" height="16" fill="none" viewBox="0 0 24 24">
@@ -96,7 +50,30 @@ export function ArticleCard({ article }: Props) {
               <span className="font-extralight text-xs">read</span>
             </span>
           )}
-          <div className="ml-4"></div>
+        </div>
+
+        {/* {JSON.stringify(article)} */}
+        {/* <p>{article.summary}</p> */}
+        <p className="pb-2 my-1 text-left text-gray-600 dark:text-gray-400  text-sm">
+          {article.summary}
+        </p>
+        <span className="text-xs flex text-gray-600 dark:text-gray-400 justify-end space-x-4 ">
+          <div className="flex  text-gray-800 dark:text-gray-200 capsize text-xs items-center ">
+            <span className=" text-xs text-gray-800 dark:text-gray-400 ">
+              {views ? new Number(views).toLocaleString() : '–––'} views.
+            </span>
+          </div>
+          <div className="  ">
+           {/* {readingTimeStats.words} mins. */}
+          </div>
+          <div>
+            {new Date(article.publishedDate).toLocaleDateString('de-AT', {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+            })}{' '}
+          </div>
+
         </span>
       </div>
     </button>
