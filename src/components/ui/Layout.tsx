c7cefb8ce82f9dc11c4461f6a085b16fe178a819
thienjs <thien.tran6@gmail.com>
2022-02-18 09:10:47 -0500

    cleanup

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -1,9 +1,7 @@
-
-import Navigation from "~/components/ui/Navigation"
-import Footer from "~/components/ui/Footer"
+import Footer from '~/components/ui/Footer'
 import { ScrollToTop } from '../features/scroll'
-import Nav from "./Nav"
-import DropMenu from "./DropMenu"
+import Nav from './Nav'
+import DropMenu from './DropMenu'
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className=" flex flex-col items-center  bg-zinc-200 dark:bg-black min-h-screen">
