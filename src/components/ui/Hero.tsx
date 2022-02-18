5f609d76420f57849c3dd6dbd43c7a8c51e40c97
thienjs <thien.tran6@gmail.com>
2022-02-17 23:35:15 -0500

    hero animation for fun

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -9,31 +9,61 @@ export default function Hero() {
     <div>
       <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse lg:text-left">
         <div className="lg:px-4 lg:mt-12 ">
-          <motion.div
-            initial="hidden"
-            animate="visible"
-            transition={{ delay: 0.2,
-            duration:0.8 }}
-            variants={{
-              hidden: {
-                opacity: 0,
-                y: 100,
-              },
-              visible: {
-                opacity: 1,
-                y: 0,
-              },
-            }}
-          >
-            <h1 className="text-xl font-semibold text-gray-900 lg:text-4xl dark:text-neutral-200 mt-40">
+          <div>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              transition={{ delay: 0.1, duration: 1 }}
+              variants={{
+                hidden: {
+                  opacity: 0,
+                  y: 100,
+                },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                },
+              }}
+              className="text-xl font-semibold text-gray-900 lg:text-4xl dark:text-neutral-200 mt-40"
+            >
               Thien Tran
-            </h1>
-            <h3 className="text-gray-500 dark:text-neutral-400 text-sm mb-2">
+            </motion.div>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              transition={{ delay: 0.2, duration: 0.9 }}
+              variants={{
+                hidden: {
+                  opacity: 0,
+                  y: 100,
+                },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                },
+              }}
+              className="text-gray-500 dark:text-neutral-400 text-sm mb-2"
+            >
               (tee-en)
-            </h3>
-          </motion.div>
+            </motion.div>
+          </div>
           <div className="mt-8 text-gray-800 dark:text-neutral-400 text-sm">
-            <p className="mb-4 ">
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              transition={{ delay: 0.4, duration: 0.8 }}
+              variants={{
+                hidden: {
+                  opacity: 0,
+                  y: 100,
+                },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                },
+              }}
+              className="mb-4 "
+            >
               Hello! I am a self taught web developer currently focused on
               typescript, nextjs, and tailwind. Most of my work is open source
               and publicly available on{' '}
@@ -45,8 +75,22 @@ export default function Hero() {
                 </a>
               </Link>
               .
-            </p>
-            <p>
+            </motion.div>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              transition={{ delay: 0.6, duration: 0.7 }}
+              variants={{
+                hidden: {
+                  opacity: 0,
+                  y: 100,
+                },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                },
+              }}
+            >
               I'm open for work or just a chat. Email me at{' '}
               <Link href="https://github.com/thienjs">
                 <a className="relative before:absolute before:bg-cyan-500 before:opacity-50 before:bottom-0 before:left-0 before:h-full before:w-full before:origin-bottom before:scale-y-[0.35] hover:before:scale-y-100 before:transition-transform before:ease-in-out before:duration-500">
@@ -56,7 +100,7 @@ export default function Hero() {
                 </a>
               </Link>
               .
-            </p>
+            </motion.div>
             <p className="mb-4"></p>
           </div>
         </div>
