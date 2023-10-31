3416124f64ea34b8e2b1b14ee6ab9b65bc1700be
thienjs <thien.tran6@gmail.com>
2023-10-31 02:55:46 -0400

    status getserversideprops

--- src/components/ui/DropMenu.tsx
+++ src/components/ui/DropMenu.tsx
@@ -197,6 +197,24 @@ export default function DropMenu() {
             </Menu.Item>
             <Menu.Item>
               {({ active }) => (
+                <Link href="/status">
+                  <a
+                    className={classNames(
+                      active
+                        ? 'bg-green-100 text-red-900 dark:bg-cyan-600 dark:text-gray-300'
+                        : 'bg-white text-zinc-700 hover:bg-gray-300 dark:bg-zinc-800 dark:text-gray-200 dark:hover:bg-zinc-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    <div className="flex flex-row">
+                      <DiscIcon className="mr-4 mt-0.5" /> Status
+                    </div>
+                  </a>
+                </Link>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
                 <Link href="/bookmarks">
                   <a
                     className={classNames(
--- src/pages/status.tsx
+++ src/pages/status.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import type { GetStaticProps } from 'next'
+import type { GetStaticProps, GetServerSideProps } from 'next'
 import { useState } from 'react'
 
 import { getStatus } from '~/lib/notion'
@@ -8,15 +8,13 @@ import { Title, Description } from '~/components/ui/typography'
 
 const StatusPage = ({ statuses }) => {
   return (
-    <p >
+    <p>
       <Title>Status</Title>
-      <p className=''>
+      <p className="">
         {statuses.map((status) => (
-          <p className='flex py-2'>
-            <p className='mr-6 text-xs pt-1'>
-            {status.date}
-            </p>
-             <p className=''>{status.title}</p>
+          <p className="flex py-2">
+            <p className="mr-6 pt-1 text-xs">{status.date}</p>
+            <p className="">{status.title}</p>
           </p>
         ))}
       </p>
@@ -24,7 +22,7 @@ const StatusPage = ({ statuses }) => {
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const statusdata = await getStatus(process.env.NOTION_STATUS_ID)
 
   const statuses = statusdata.map((status: any) => ({
