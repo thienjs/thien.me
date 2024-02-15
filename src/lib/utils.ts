a498557756492f3da71f126a350dc18c37d6e75a
thienjs <thien.tran6@gmail.com>
2024-02-15 01:19:12 -0500

    update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,34 +1,6 @@
-import { useState } from 'react'
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-export function useFormFields<T>(
-  initialValues: T
-): [T, (event: React.ChangeEvent<HTMLInputElement>) => void, () => void] {
-  const [values, setValues] = useState<T>(initialValues)
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.persist()
-    const { target } = event
-    const { name, value } = target
-    setValues({ ...values, [name]: value })
-  }
-  const resetFormFields = () => setValues(initialValues)
-  return [values, handleChange, resetFormFields]
-}
-
-export function jsonResponse(status: number, data: any, init?: ResponseInit) {
-  return new Response(JSON.stringify(data), {
-    ...init,
-    status,
-    headers: {
-      ...init?.headers,
-      'Content-Type': 'application/json',
-    },
-  })
-}
-
-export async function fetcher<JSON = any>(
-  input: RequestInfo,
-  init?: RequestInit
-): Promise<JSON> {
-  const res = await fetch(input, init)
-  return res.json()
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
