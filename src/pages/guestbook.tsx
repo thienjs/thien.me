5894c613c4c00221b30ba390631b6819889e4354
thienjs <thien.tran6@gmail.com>
2024-02-15 17:35:25 -0500

    test guestbook with update deps

--- src/pages/guestbook.tsx
+++ src/pages/guestbook.tsx
@@ -5,9 +5,23 @@ import { GuestbookContent } from '~/components/guestbook'
 import { Title, Description } from '~/components/ui/typography'
 import { motion } from 'framer-motion'
 
+export const getStaticProps = async () => {
+  const data = await fetchGuestbookEntries()
 
+  const entries = data.map((entry) => ({
+    id: entry.id,
+    body: entry.body,
+    createdBy: entry.createdBy.toString(),
+    createdAt: entry.createdAt.toString(),
+  }))
+  return {
+    props: { entries, revalidate: 60 }, //revalidate every minute
+  }
+}
 
-const GuestBookPage = () => {
+const GuestBookPage = ({
+  entries,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <Title>Guestbook</Title>
@@ -29,7 +43,8 @@ const GuestBookPage = () => {
           },
         }}
       >
-        <p>hello</p>
+        <ContentPopover />
+        <GuestbookContent data={entries} />
       </motion.div>
     </>
   )
