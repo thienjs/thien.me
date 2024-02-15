2e30c6dc9568e5e52ad7bbd1eb9e8431580281b7
thienjs <thien.tran6@gmail.com>
2024-02-14 23:31:15 -0500

    broken

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 const hello = (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'Thien Tran' })
 }
 
 export default hello
