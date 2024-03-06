70415d0eb069c12249b4d4f795ae7c7825ff37ea
thienjs <thien.tran6@gmail.com>
2024-03-06 02:31:55 -0500

    delete

--- src/hooks/getUserSession.tsx
+++
@@ -1,7 +0,0 @@
-import { UserData } from "@types";
-import { useSession } from "next-auth/react";
-
-export function getUserSession(): UserData {
-    let { data } = useSession();
-    return data?.userData!;
-}
